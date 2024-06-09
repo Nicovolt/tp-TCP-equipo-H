fdfc999a4c7c761facae66324d36758a96e47124
Yorgenis <91138262+yorgenis-19@users.noreply.github.com>
2024-06-09 19:59:40 -0300

    Pantalla Nuevo articulo
    se creo la pantalla para cargar un articulo nuevo

---
+++ Ecommerce/tp-TCP-equipo-H/NuevoArticulo.aspx.designer.cs
@@ -0,0 +1,17 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace tp_TCP_equipo_H
+{
+
+
+    public partial class NuevoArticulo
+    {
+    }
+}
