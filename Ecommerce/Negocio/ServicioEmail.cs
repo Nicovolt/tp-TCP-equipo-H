60a49af8e72f9d48647315a0a882cc2aaecc26d5
Yorgenis <91138262+yorgenis-19@users.noreply.github.com>
2024-06-27 08:50:58 -0300

    Clase servicioEmail
    se creo la la clese servicioemail para el envio del mismo cuando se realize una compra y otras cosas mas

---
+++ Ecommerce/Negocio/ServicioEmail.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    internal class ServicioEmail
+    {
+        private SmtpClient server;
+        private MailMessage mail;
+
+        public ServicioEmail()
+        {
+            server = new SmtpClient();
+            server.Credentials = new NetworkCredential("90a1b3d2a55fe2", "8f9b2d2ad821fc");
+            server.EnableSsl = true;
+            server.Port = 587;
+            server.Host = "sandbox.smtp.mailtrap.io";
+        }
+
+        public void armarCorreo(string destino, string asunto, string cuerpo)
+        {
+            mail = new MailMessage();
+            mail.From = new MailAddress("noresponder@tienda30.com");
+            mail.To.Add(destino);
+            mail.Subject = asunto;
+            mail.Body = cuerpo;
+        }
+
+        public void enviarMail()
+        {
+            try
+            {
+                server.Send(mail);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
